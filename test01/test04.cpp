991b90164b82d5a9895c069f8f153a4b8d43bd40
Gimbol4 <Shishkanov.yd@gmail.com>
2026-04-27 17:48:56 +0300

    5 test

--- test01/test04.cpp
+++ test01/test04.cpp
@@ -1,16 +1,17 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-	int number = 10;
-	int* ptr = NULL;
-	ptr = &number;
-
-	cout << "0) " << ptr << endl;
-	cout << "1) " << ++ptr << endl;
-	cout << "2) " << ++ptr << endl;
-	cout << "3) " << ++ptr << endl;
-	cout << "4) " << ++ptr << endl;
-
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	int* ptr = NULL;
+//
+//	int number = 10;
+//	ptr = &number;
+//
+//	cout << "Before number = " << number << endl;
+//
+//	*ptr = *ptr + 5;
+//
+//	cout << "After: number = " << number << endl;
+//
+//	return 0;
+//}
---
+++ test01/test06.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	short number = 10;
+	short* ptr1 = NULL;
+	short* ptr2 = NULL;
+	ptr1 = &number;
+	ptr2 = ptr1 + 10;
+
+
+	cout << "result: " << (ptr2 - ptr1) << endl;
+
+	return 0;
+}
