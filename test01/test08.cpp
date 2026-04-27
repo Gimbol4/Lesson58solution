448e7957185914264cf4c77ec185443931174d3d
Gimbol4 <Shishkanov.yd@gmail.com>
2026-04-27 18:44:07 +0300

    test08

--- test01/test08.cpp
+++ test01/test08.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
 
-void swap(int* a, int* b) {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
+void swap(int& a, int& b) {
+	int temp = a;
+	a = b;
+	b = temp;
 }
 
 int main() {
@@ -12,7 +12,7 @@ int main() {
 
 	cout << "Before: x = " << x << ", y = " << y << endl;
 
-	swap(&x, &y);
+	swap(x, y);
 
 	cout << "After: x = " << x << ", y = " << y << endl;
 
